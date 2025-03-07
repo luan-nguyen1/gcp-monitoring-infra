adbf5d7dbc20154b0c51ce2d63dab3a2d55c2993
Luân Nguyễn <ramenchampion9@gmail.com>
2025-03-07 18:40:40 +0100

    api

---
+++ modules/apis/main.tf
@@ -0,0 +1,25 @@
+# modules/apis/main.tf
+resource "google_project_service" "cloud_resource_manager" {
+  project = var.project_id
+  service = "cloudresourcemanager.googleapis.com"
+}
+
+resource "google_project_service" "compute" {
+  project = var.project_id
+  service = "compute.googleapis.com"
+}
+
+resource "google_project_service" "container" {
+  project = var.project_id
+  service = "container.googleapis.com"
+}
+
+resource "google_project_service" "artifact_registry" {
+  project = var.project_id
+  service = "artifactregistry.googleapis.com"
+}
+
+resource "google_project_service" "service_usage" {
+  project = var.project_id
+  service = "serviceusage.googleapis.com"
+}
---
+++ modules/apis/variables.tf
@@ -0,0 +1,5 @@
+# modules/apis/variables.tf
+variable "project_id" {
+  type        = string
+  description = "The GCP project ID where APIs will be enabled"
+}
